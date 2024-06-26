7a7981e5627645d8055b288addb5055c9f5ba421
BerkeAnna <berke.anna0803@gmail.com>
2024-06-26 13:20:25 +0200

    basics

---
+++ habitTracker/components/Task.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Task = (props) => {
+  return (
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <View style={[styles.square, props.completed && styles.completedSquare]}></View>
+        <Text style={styles.itemText}>{props.text}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#FFF',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap'
+  },
+  square: {
+    width: 24,
+    height: 24,
+    borderWidth: 2,
+    borderColor: '#FF5F94',
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  completedSquare: {
+    backgroundColor: '#FF5F94', // Blue color when completed
+  },
+  itemText: {
+    maxWidth: '80%',
+  },
+});
+
+export default Task;
